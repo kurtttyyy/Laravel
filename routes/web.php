96bbbda90673de1d0e05a46a7869af33d69bbba8
Fallaria <chrislfallaria@gmail.com>
2026-01-14 15:34:12 +0800

    sin

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
+use App\Http\Controllers\GuestPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('applicationSteps');
+    return view('guest.index');
 });
 
-Route::get('/index', function () {
-    return view('applicationSteps');
-});
-
-Route::get('/application', function () {
-    return view('application');
-});
-
-Route::get('/jobOpen', function () {
-    return view('jobOpen');
-});
-
-Route::get('/ApplicationSteps', function () {
-    return view('applicationSteps');
+Route::controller(GuestPageController::class)->group(function () {
+    Route::get('/application', 'display_application')->name('guest.application');
+    Route::get('/application/procedure', 'display_steps')->name('guest.applicationSteps');
+    Route::get('/index', 'display_index')->name('guest.index');
+    Route::get('/job/available', 'display_job')->name('guest.jobOpen');
 });
